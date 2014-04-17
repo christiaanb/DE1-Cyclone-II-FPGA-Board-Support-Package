431f455ea10959f2205e95bf85ee3660f264214d
Christiaan Baaij <christiaan.baaij@gmail.com>
2014-04-17 17:10:27 +0200

    update mixer to clash2

--- Mixer.hs
+++ Mixer.hs
@@ -1,14 +1,16 @@
 module Mixer where
 
-import CLasH.HardwareTypes
+import CLaSH.Prelude
 
-mixer :: Comp ((Signed D18, Signed D16), (Signed D18, Signed D16)) (Signed D18, Signed D18)  
-mixer = arr mixerT
+mixer :: ( (Signal (Signed 18), Signal (Signed 16))
+         , (Signal (Signed 18), Signal (Signed 16)))
+      -> (Signal (Signed 18), Signal (Signed 18))
+mixer (l,r) = unpack (mixerT <$> pack (pack l, pack r))
 
-mixerT :: ((Signed D18, Signed D16), (Signed D18, Signed D16)) -> (Signed D18, Signed D18)  
-mixerT ((lChannel1,lChannel2),(rChannel1,rChannel3)) = (lChannelOut,rChannelOut)
+mixerT :: ((Signed 18, Signed 16), (Signed 18, Signed 16)) -> (Signed 18, Signed 18)
+mixerT ((lChannel1,lChannel2),(rChannel1,rChannel2)) = (lChannelOut,rChannelOut)
   where
-    lChannel2'  = bv2s ((s2bv lChannel2) <++> (vcopyn d2 Low))
-    rChannel2'  = bv2s ((s2bv lChannel2) <++> (vcopyn d2 Low))
+    lChannel2'  = fromBV ((toBV lChannel2) <++> (vcopy d2 L))
+    rChannel2'  = fromBV ((toBV rChannel2) <++> (vcopy d2 L))
     lChannelOut = (shiftR lChannel1 1) + (shiftR lChannel2' 1)
     rChannelOut = (shiftR rChannel1 1) + (shiftR rChannel2' 1)
