a12576acda3c1cfcc085e5463aed988249922f1f
Christiaan Baaij <christiaan.baaij@gmail.com>
2014-09-03 11:21:53 +0200

    Update new I2C bit controller

--- I2C/MasterBit.hs
+++ I2C/MasterBit.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
-module MasterBit where
+module I2C.MasterBit where
 
 import CLaSH.Prelude
 import Control.Lens
@@ -11,14 +11,16 @@ import Utils
 
 data BusStatusCtrl
   = BusStatusCtrl
-  { _cSCL, _cSDA    :: BitVector 2 -- capture SDA and SCL
-  , _filterCnt      :: Unsigned 14 -- clock divider for filter
-  , _fSCL, _fSDA    :: BitVector 2 -- filter inputs for SCL and SDA
-  , _startCondition :: Bool        -- start detected
-  , _stopCondition  :: Bool        -- stop detected
-  , _ibusy          :: Bool        -- internal busy signal
-  , _cmdStop        :: Bool        -- STOP command
-  , _ial            :: Bool        -- internal arbitration lost signal
+  { _cI2C           :: Vec 2 I2CIPair -- capture SDA and SCL
+  , _filterCnt      :: Unsigned 14    -- clock divider for filter
+  , _fI2C           :: Vec 3 I2CIPair -- filtered inputs for SCL and SDA
+  , _sI2C           :: I2CIPair       -- synchronised SCL and SDA
+  , _dI2C           :: I2CIPair       -- delayed version of 'sI2C'
+  , _startCondition :: Bool           -- start detected
+  , _stopCondition  :: Bool           -- stop detected
+  , _ibusy          :: Bool           -- internal busy signal
+  , _cmdStop        :: Bool           -- STOP command
+  , _ial            :: Bool           -- internal arbitration lost signal
   }
 
 makeLenses ''BusStatusCtrl
@@ -31,20 +33,25 @@ data BitStateMachine
     WriteA | WriteB | WriteC | WriteD
   deriving Eq
 
-data MasterBitS
-  = MasterBitS
+data StateMachine
+  = StateMachine
   { _isclOen, _isdaOen   :: Bool        -- Internal I2C lines
   , _sdaChk              :: Bool        -- check SDA status (multi-master arbiter)
   , _cmdAck              :: Bool        -- command completed
-  , _dsclOen             :: Bool        -- delayed sclOen signal
-  , _sI2C                :: I2CIPair    -- synchronised SCL and SDA
-  , _dI2C                :: I2CIPair    -- delayed version of sI2C
-  , _clkEn               :: Bool        -- statemachine clock enable
+  , _cState              :: BitStateMachine
+  }
+
+makeLenses ''StateMachine
+
+data MasterBitS
+  = MasterBitS
+  { _dsclOen             :: Bool        -- delayed sclOen signal
   , _slaveWait           :: Bool        -- clock generation signals
   , _cnt                 :: Unsigned 16 -- clock devider counter
+  , _clkEn               :: Bool        -- statemachine clock enable
   , _dout                :: Bit
   , _busState            :: BusStatusCtrl
-  , _cState              :: BitStateMachine
+  , _stateMachine        :: StateMachine
   }
 
 makeLenses ''MasterBitS
@@ -52,171 +59,265 @@ makeLenses ''MasterBitS
 --startState :: MasterBitS
 --startState = MasterBitS
 --           { _dscl_oen   = False
---           , _iscl_oen   = True
+--           , _isclOen   = True
 --           , _slave_wait = False
 --           , _dSCL       =
 --           }
-topEntity = busStatusCtrl
+topEntity = nextStateDecoder
 
-masterBitCtrlT :: MasterBitS
-               -> Unsigned 16
+masterBitCtrlT :: Unsigned 16
                -> I2Ccommand
                -> Bit
                -> I2CIPair
-               -> (MasterBitS, MasterBitS)
-masterBitCtrlT s clkCnt cmd dIn i2ci = (`runState` s) $ do
+               -> MasterBitS
+               -> ((), MasterBitS)
+masterBitCtrlT clkCnt cmd dIn i2ci = runState $ do
   (MasterBitS {..}) <- get
+  -- Extract substate information
+  let sSCL      = _scli (_sI2C _busState)
+      dSCL      = _scli (_dI2C _busState)
+      sSDA      = _sdai (_sI2C _busState)
+      isclOen   = _isclOen _stateMachine
+      isdaOen   = _isdaOen _stateMachine
+      sdaChk    = _sdaChk _stateMachine
+      cState    = _cState _stateMachine
 
   -- whenever the slave is not ready it can delay the cycle by pulling SCL low
   -- delay scl_oen
-  dsclOen .= _isclOen
+  dsclOen .= isclOen
 
   -- slave_wait is asserted when master wants to drive SCL high, but the slave pulls it low
   -- slave_wait remains asserted until the slave releases SCL
-  let masterSclHigh = _isclOen && not _dsclOen
-  slaveWait .= ((masterSclHigh || _slaveWait) && _scli _sI2C == 0)
+  let masterSclHigh = isclOen && not _dsclOen
+  slaveWait .= ((masterSclHigh || _slaveWait) && sSCL == 0)
 
   -- master drives scl high, but another master pulls it low
   -- master start counting down its low cycle now (clock synchronisation)
-  let sclSync = _scli _dI2C == 1 &&
-                _scli _sI2C == 0 &&
-                _isclOen
+  let sclSync = sSCL == 1 &&
+                dSCL == 0 &&
+                isclOen
 
   -- generate clk enable signal
   if sclSync then do
-     cnt   .= clkCnt
+     cnt .= clkCnt
      clkEn .= True
   else if _slaveWait then do
      clkEn .= False
   else do
-     cnt   -= 1
+     cnt -= 1
      clkEn .= False
 
   -- generate bus status controller
-  -- busStatusCtrl clkCnt cmd i2ci
-
-  get
-
-busStartState
-  = BusStatusCtrl
-  { _cSCL = 0, _cSDA = 0
-  , _filterCnt = 0
-  , _fSCL = maxBound, _fSDA = maxBound
-  , _startCondition = False
-  , _stopCondition = False
-  , _ibusy = False
-  , _cmdStop = False
-  , _ial = False
-  }
-
-busStatusCtrl :: Unsigned 16
-              -> I2Ccommand
-              -> I2CIPair
-              -> State MasterBitS ()
-busStatusCtrl clkCnt cmd (I2CIPair sclI sdaI) = do
-  (MasterBitS {..}) <- get
-  let (BusStatusCtrl {..}) = _busState
-
-  -- zoom busState $ do
-    -- capture SCL and SDA
-  busState.cSCL .= lsb _cSCL ++# sclI
-  busState.cSDA .= lsb _cSDA ++# sdaI
-
-  -- filter SCL and SDA; (attempt to) remove glitches
-  if _filterCnt == 0 then do
-     busState.filterCnt .= unpack (fst (split clkCnt))
-  else do
-     busState.filterCnt -= 1
-
-  when (_filterCnt == 0) $ do
-    busState.fSCL %= (<<# msb _cSCL)
-    busState.fSDA %= (<<# msb _cSDA)
-
-  -- generated filtered SCL and SDA signals
-  (sI2C.scli) .= ((_fSCL!2 .&. _fSCL!1) .|.
-                  (_fSCL!2 .&. _fSCL!0) .|.
-                  (_fSCL!1 .&. _fSCL!0))
-
-  (sI2C.sdai) .= ((_fSDA!2 .&. _fSDA!1) .|.
-                  (_fSDA!2 .&. _fSDA!0) .|.
-                  (_fSDA!1 .&. _fSDA!0))
-
-  -- Delayed version of sI2C
-  dI2C .= _sI2C
-
-  let sSDA = _sdai _sI2C
-      dSDA = _sdai _dI2C
-      sSCL = _scli _sI2C
-      dSCL = _scli _dI2C
-
-  -- zoom busState $ do
-  -- detect start condition => detect falling edge on SDA while SCL is high
-  -- detect stop condition => detect rising edge on SDA while SCL is high
-  busState.startCondition .= ((sSDA == 0 && dSDA == 1) && (sSCL == 1))
-  busState.stopCondition  .= ((sSDA == 1 && dSDA == 0) && (sSCL == 1))
-
-  -- generate i2c-bus busy signal
-  busState.ibusy .= ((_startCondition || _ibusy) && not _stopCondition)
-
-  -- generate arbitration lost signal
-  -- arbitration lost when:
-  -- 1) master drives SDA high, but the i2c bus is low
-  -- 2) stop detected while not requested (detect during 'idle' state)
-  when _clkEn (busState.cmdStop .= (cmd == I2C_Stop))
-
-  let masterHighBusLow = _sdaChk && sSDA == 0 && _isdaOen
-  if _cState == Idle then
-     busState.ial .= (masterHighBusLow || (_stopCondition && not _cmdStop))
-  else
-     busState.ial .= masterHighBusLow
+  zoom busState (busStatusCtrl clkCnt cmd sdaChk isdaOen _clkEn cState i2ci)
 
   -- generate dout signal, store out on rising edge of SCL
   when (sSCL == 1 && dSCL == 0) (dout .= sSDA)
 
-nextStateDecoder cmd = do
-  (MasterBitS {..}) <- get
+  -- generate statemachine
+  -- zoom stateMachine (nextStateDecoder cmd _clkEn)
+
+busStartState
+  = BusStatusCtrl
+  { _cI2C           = repeat (I2CIPair 0 0)
+  , _filterCnt      = 0
+  , _fI2C           = repeat (I2CIPair 1 1)
+  , _sI2C           = (I2CIPair 1 1)
+  , _dI2C           = (I2CIPair 1 1)
+  , _startCondition = False
+  , _stopCondition  = False
+  , _ibusy          = False
+  , _cmdStop        = False
+  , _ial            = False
+  }
+
+busStatusCtrl :: Unsigned 16
+              -> I2Ccommand
+              -> Bool
+              -> Bool
+              -> Bool
+              -> BitStateMachine
+              -> I2CIPair
+              -> State BusStatusCtrl ()
+busStatusCtrl clkCnt cmd sdaChk isdaOen clkEn cState i2ci  = do
+    (BusStatusCtrl {..}) <- get
+
+    -- capture SCL and SDA
+    cI2C .= (i2ci +>> _cI2C)
+
+    -- filter SCL and SDA (attempt to) remove glitches
+    if _filterCnt == 0 then do
+       filterCnt .= unpack (fst (split clkCnt))
+    else do
+       filterCnt -= 1
+
+    when (_filterCnt == 0) $
+      fI2C .= (last _cI2C +>> _fI2C)
+
+    -- generated filtered SCL and SDA signals
+    (sI2C.scli) .= filterT (map _scli _fI2C)
+    (sI2C.sdai) .= filterT (map _sdai _fI2C)
+
+    -- Delayed version of sI2C
+    dI2C .= _sI2C
+
+    let sSDA = _sdai _sI2C
+        dSDA = _sdai _dI2C
+        sSCL = _scli _sI2C
+
+    -- zoom busState $ do
+    -- detect start condition -> do detect falling edge on SDA while SCL is high
+    -- detect stop condition -> do detect rising edge on SDA while SCL is high
+    startCondition .= ((sSDA == 0 && dSDA == 1) && (sSCL == 1))
+    stopCondition  .= ((sSDA == 1 && dSDA == 0) && (sSCL == 1))
+
+    -- generate i2c-bus busy signal
+    ibusy .= ((_startCondition || _ibusy) && not _stopCondition)
+
+    -- generate arbitration lost signal
+    -- arbitration lost when:
+    -- 1) master drives SDA high, but the i2c bus is low
+    -- 2) stop detected while not requested (detect during 'idle' state)
+    when clkEn (cmdStop .= (cmd == I2C_Stop))
+
+    let masterHighBusLow = sdaChk && sSDA == 0 && isdaOen
+    if cState == Idle then
+       ial .= (masterHighBusLow || (_stopCondition && not _cmdStop))
+    else
+       ial .= masterHighBusLow
+  where
+    filterT f = (f!!2 .&. f!!1) .|.
+                (f!!2 .&. f!!0) .|.
+                (f!!1 .&. f!!0)
+
+nextStateDecoder :: I2Ccommand
+                 -> Bool
+                 -> Bit
+                 -> StateMachine
+                 -> ((),StateMachine)
+nextStateDecoder cmd clkEn dIn = runState $ do
+  (StateMachine {..}) <- get
 
   -- default no acknowledge
   cmdAck .= False
 
-  when _clkEn $ do
+  when clkEn $ do
     case _cState of
+      -- idle
       Idle -> do
-        case cmd of
-          I2C_Start -> cState .= StartA
-          I2C_Stop  -> cState .= StopA
-          I2C_Write -> cState .= WriteA
-          I2C_Read  -> cState .= ReadA
-          _         -> cState .= Idle -- NOP command
+          case cmd of
+            I2C_Start -> cState .= StartA
+            I2C_Stop  -> cState .= StopA
+            I2C_Write -> cState .= WriteA
+            I2C_Read  -> cState .= ReadA
+            _         -> cState .= Idle -- NOP command
 
-        sdaChk .= False
+          sdaChk .= False
 
+      -- start
       StartA -> do
-        cState  .= StartB
-        isdaOen .= True  -- set SDA high
-        sdaChk  .= False -- don't check SDA
+          cState  .= StartB
+          isdaOen .= True  -- set SDA high
+          sdaChk  .= False -- don't check SDA
 
       StartB -> do
-        cState  .= StartC
-        isclOen .= True  -- set SCL high
-        isdaOen .= True  -- keep SDA high
-        sdaChk  .= False -- don't check SDA
+          cState  .= StartC
+          isclOen .= True  -- set SCL high
+          isdaOen .= True  -- keep SDA high
+          sdaChk  .= False -- don't check SDA
 
       StartC -> do
-        cState  .= StartD
-        isclOen .= True  -- keep SCL high
-        isdaOen .= False -- set SDA low
-        sdaChk  .= False -- don't check SDA
+          cState  .= StartD
+          isclOen .= True  -- keep SCL high
+          isdaOen .= False -- set SDA low
+          sdaChk  .= False -- don't check SDA
 
       StartD -> do
-        cState  .= StartE
-        isclOen .= True  -- keep SCL high
-        isdaOen .= False -- keep SDA low
-        sdaChk  .= False -- don't check SDA
+          cState  .= StartE
+          isclOen .= True  -- keep SCL high
+          isdaOen .= False -- keep SDA low
+          sdaChk  .= False -- don't check SDA
 
       StartE -> do
-        cState  .= Idle
-        cmdAck  .= True  -- command completed
-        isclOen .= False -- set SCL low
-        sdaChk  .= False -- don't check SDA
+          cState  .= Idle
+          cmdAck  .= True  -- command completed
+          isclOen .= False -- set SCL low
+          sdaChk  .= False -- don't check SDA
 
+      -- stop
+      StopA -> do
+          cState  .= StopB
+          isclOen .= False -- keep SCL low
+          isdaOen .= False -- set SDA low
+          sdaChk  .= False -- don't check SDA
+
+      StopB -> do
+          cState  .= StopC
+          isclOen .= True -- set SCL high
+          isdaOen .= False -- keep SDA low
+          sdaChk  .= False -- don't check SDA
+
+      StopC -> do
+          cState  .= StopD
+          isclOen .= True -- keep SCL high
+          isdaOen .= False -- keep SDA low
+          sdaChk  .= False -- don't check SDA
+
+      StopD -> do
+          cState  .= Idle
+          cmdAck .= True -- command completed
+          isclOen .= True -- keep SCL high
+          isdaOen .= True -- set SDA high
+          sdaChk  .= False -- don't check SDA
+
+      -- read
+      ReadA -> do
+          cState  .= ReadB
+          isclOen .= False -- keep SCL low
+          isdaOen .= True -- tri-state SDA
+          sdaChk  .= False -- don't check SDA
+
+      ReadB -> do
+          cState  .= ReadC
+          isclOen .= True -- set SCL high
+          isdaOen .= True -- tri-state SDA
+          sdaChk  .= False -- don't check SDA
+
+      ReadC -> do
+          cState  .= ReadD
+          isclOen .= True -- keep SCL high
+          isdaOen .= True -- tri-state SDA
+          sdaChk  .= False -- don't check SDA
+
+      ReadD -> do
+          cState  .= Idle
+          cmdAck  .= True -- command completed
+          isclOen .= False -- set SCL low
+          isdaOen .= True -- tri-state SDA
+          sdaChk  .= False -- don't check SDA
+
+      -- write
+      WriteA -> do
+          cState  .= WriteB
+          isclOen .= False -- keep SCL low
+          isdaOen .= (dIn == 1) -- set SDA
+          sdaChk  .= False -- don't check SDA (SCL low)
+
+      WriteB -> do
+          cState  .= WriteC
+          isclOen .= True -- set SCL high
+          isdaOen .= (dIn == 1) -- keep SDA
+          sdaChk  .= False -- don't check SDA yet
+                           -- Allow some more time for SDA and SCL to settle
+
+      WriteC -> do
+          cState  .= WriteD
+          isclOen .= True -- keep SCL high
+          isdaOen .= (dIn == 1) -- keep SDA
+          sdaChk  .= True -- check SDA
+
+      WriteD -> do
+          cState  .= Idle
+          cmdAck  .= True -- command completed
+          isclOen .= False -- set SCL low
+          isdaOen .= (dIn == 1) -- keep SDA
+          sdaChk  .= False -- don't check SDA (SCL low)
